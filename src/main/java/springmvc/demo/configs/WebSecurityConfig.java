ac76d782c893fbeec217c8f3d93076e2b7e4f3bc
trachpro <tu.phamminh.2207@gmail.com>
2018-11-27 22:20:55 +0700

    redeploy

--- src/main/java/springmvc/demo/configs/WebSecurityConfig.java
+++ src/main/java/springmvc/demo/configs/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package springmvc.demo.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
