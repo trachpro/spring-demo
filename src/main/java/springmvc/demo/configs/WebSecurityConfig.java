6f21f30069321869561fe7d28ac861dd939097a9
trachpro <tu.phamminh.2207@gmail.com>
2018-12-05 15:15:51 +0700

    restart

--- src/main/java/springmvc/demo/configs/WebSecurityConfig.java
+++ src/main/java/springmvc/demo/configs/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        System.out.println("the second");
+        System.out.println("the second 1");
         http
                 .cors().and()
                 .csrf().disable().authorizeRequests()
